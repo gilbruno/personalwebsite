866b3ca78c1db8c26e5dacd5020a961e63ca728a
Gilles Bruno <gilles_bruno@gmx.fr>
2023-04-24 15:30:46 +0200

    Add sanity schema

--- sanity.config.ts
+++ sanity.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from 'sanity'
 import { deskTool } from 'sanity/desk'
+import project from './sanity/schemas/project-schema'
 
 const config = defineConfig({
     projectId: '6hwupml2',
@@ -7,7 +8,8 @@ const config = defineConfig({
     title: 'My personal website',
     apiVersion: '2023-03-04',
     basePath: '/admin',
-    plugins: [deskTool()]
+    plugins: [deskTool()],
+    schema: { types: [project]}
 })
 
 export default config
