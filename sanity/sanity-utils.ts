6f1df260a8251151dc6aa599dbb1f5d7ffd61214
Gilles Bruno <gilles_bruno@gmx.fr>
2023-04-24 19:22:47 +0200

    Fetch data from sanity admin

---
+++ sanity/sanity-utils.ts
@@ -0,0 +1,23 @@
+import { createClient, groq } from "next-sanity";
+import { Project } from "../types/Project";
+
+export async function getProjects(): Promise<Project[]> {
+    const client = createClient({
+        projectId: '6hwupml2',
+        dataset: 'production',
+        apiVersion: '2023-03-04'
+    })
+
+    return client.fetch(
+        groq`*[_type == "project"]{
+            _id,
+            _createdAt,
+            name,
+            "slug": slug.current,
+            "image": image.asset->url,
+            url,
+            content
+        }`
+    )
+}
+
